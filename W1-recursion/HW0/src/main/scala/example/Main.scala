5cf099aa318c9b547a861abd02dedb00cbd6b1ad
Alexandrov Konstantin <mgbt1024@gmail.com>
2020-02-01 18:12:19 +0300

    add sum() and max()

--- W1-recursion/HW0/src/main/scala/example/Main.scala
+++ W1-recursion/HW0/src/main/scala/example/Main.scala
@@ -1,5 +1,10 @@
 package example
+import Lists._
 
-object gintMain {
+object Main extends App {
+  println("HW0")
+  println(sumTR(List(1,2,3)))
 
+  println("Max (1,3,2)")
+  println(max(List(1,3,2)))
 }
